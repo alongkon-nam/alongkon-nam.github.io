d5a63c38146db1e26f22ce9b5898361c1854bfe6
alongkon-nam <88099129+alongkon-nam@users.noreply.github.com>
2021-08-01 17:42:39 +0700

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAU9JIZ53AP3hLt76Y5HKXnOQDPByUuGKU",
+    authDomain: "otop-findfolk.firebaseapp.com",
+    databaseURL: "https://otop-findfolk-default-rtdb.firebaseio.com",
+    projectId: "otop-findfolk",
+    storageBucket: "otop-findfolk.appspot.com",
+    messagingSenderId: "1059997449921",
+    appId: "1:1059997449921:web:20f18aedf1501061039e40",
+    measurementId: "G-ET7YKL27S2"
+  }
+};
